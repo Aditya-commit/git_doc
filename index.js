93a98505e51401a4634fffdef4f92205d485e68c
Aditya Tomar <aditya@gmail.com>
2021-09-02 18:29:19 +0530

    index.js file is also commited file with some advance features

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const makeAlert = () => {
 	alert('Thank You for using GIT');
 }
+const displayName = () => {
+	const name = document.getElementsByName('text')[0].value;
+	
+	let div_el = document.createElement('DIV');
+
+	console.log(name);
+
+	div_el.innerHTML = `<h4>${name}</h4>`;
+
+	document.getElementsByTagName('body')[0].appendChild(div_el);
+}
